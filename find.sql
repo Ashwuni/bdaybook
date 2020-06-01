5d90edbb62e8d2769edad71fcf0e13b64b4be507
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-01 01:41:08 +0000

    Commit changes

--- find.sql
+++ find.sql
@@ -1,7 +1,9 @@
--- Name: YourNameHere
--- WatIAM username: IDhere
+-- Name: Ashwuni Kumar
+-- WatIAM username: a255kuma
 -- 
 -- Details: This file demonstrates how to query the bdaybook to
 --          find birthdays matching a given name.
 --          
--- Warning: Read the homework for detailed instructions.         
+-- Warning: Read the homework for detailed instructions.    
+USE bdaybook;
+select * from bdaybook where firstname='william' or lastname='william';
