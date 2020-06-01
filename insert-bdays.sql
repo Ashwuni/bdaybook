f6865282073071e80a65455874ad35977377070a
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-01 01:41:30 +0000

    Commit changes

--- insert-bdays.sql
+++ insert-bdays.sql
@@ -1,8 +1,17 @@
--- Name: YourNameHere
--- WatIAM username: IDhere
+-- Name: Ashwuni Kumar
+-- WatIAM username: a255kuma
 -- 
 -- Details: This file inserts some example data into the database.
 --          Example data can be used for testing other SQL queries
 --          and/or testing an actual application.
 --          
 -- Warning: Read the homework for detailed instructions.
+USE bdaybook;
+INSERT INTO bdaybook(firstname, lastname, birthdate)
+VALUES ("Madonna", "Ciccone", '1958-8-16'),
+("William", "Gates", '1955-10-28'),
+("Natalie", "Hershlag", '1981-06-09'),
+("Lawrence", "Ellison", '1944-08-17'),
+("William", "Pitt", '1963-12-18'),
+("Michael", "Stonebraker", '1943-10-11'),
+("Ashwuni", "Kumar", '2000-04-14');
