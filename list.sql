5f9617d7b47a029d25b8255a7c5fd1a56b7527ce
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-01 01:41:46 +0000

    Commit changes

--- list.sql
+++ list.sql
@@ -1,7 +1,9 @@
--- Name: YourNameHere
--- WatIAM username: IDhere
+-- Name: Ashwuni Kumar
+-- WatIAM username: a255kuma
 -- 
 -- Details: This file demonstrates how to query the bdaybook to
 --          list all birthdays.
 --          
 -- Warning: Read the homework for detailed instructions.
+USE bdaybook;
+select * from bdaybook;
