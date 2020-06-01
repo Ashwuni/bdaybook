fa61e8fffd23edf31639b6d7bcd56338f63f3d0f
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-01 01:42:14 +0000

    Commit changes

--- reset.sql
+++ reset.sql
@@ -1,6 +1,10 @@
--- Name: YourNameHere
--- WatIAM username: IDhere
+-- Name: Ashwuni Kumar
+-- WatIAM username: a255kuma
 -- 
 -- Details: This file resets the database.
 --          
 -- Warning: Read the homework for detailed instructions.
+-- 
+USE bdaybook;
+DROP TABLE bdaybook;
+DROP DATABASE bdaybook;
