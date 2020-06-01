e3c71592e6127897c5a21dea62ecf9197acb9da3
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-01 01:40:47 +0000

    Commit changes

--- create.sql
+++ create.sql
@@ -1,6 +1,15 @@
--- Name: YourNameHere
--- WatIAM username: IDhere
+-- Name: Ashwuni Kumar
+-- WatIAM username: a255kuma
 -- 
 -- Details: This file creates the bdaybook database and its tables.
 --          
 -- Warning: Read the homework for detailed instructions.
+CREATE DATABASE bdaybook;
+USE bdaybook;
+CREATE TABLE bdaybook(
+    id INT(8) UNSIGNED NOT NULL auto_increment,
+    firstname VARCHAR(255) NOT NULL, 
+    lastname VARCHAR(255) NOT NULL,
+    birthdate DATE NOT NULL,
+    PRIMARY KEY(id)
+) AUTO_INCREMENT=1;
